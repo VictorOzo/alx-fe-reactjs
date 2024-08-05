520e499868751f2c1bfc0ca0cb99cf34f9d5e1eb
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-05 20:08:53 +0100

    Third commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
+import Header from './components/Header';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
+    <>
+      <Header/>
 			<div>
 				<a href='https://vitejs.dev' target='_blank'>
 					<img src={viteLogo} className='logo' alt='Vite logo' />
@@ -16,7 +20,8 @@ function App() {
 				<a href='https://react.dev' target='_blank'>
 					<img src={reactLogo} className='logo react' alt='React logo' />
 				</a>
-			</div>
+      </div>
+      <MainContent />
 			<h1>Vite + React</h1>
 			<div className='card'>
 				<button onClick={() => setCount((count) => count + 1)}>
@@ -30,6 +35,7 @@ function App() {
 			<p className='read-the-docs'>
 				Click on the Vite and React logos to learn more
 			</p>
+			<Footer />
 		</>
 	);
 }
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+	<header>
+		<h1>My Favorite Cities</h1>
+	</header>;
+}
+
+export default Header;
