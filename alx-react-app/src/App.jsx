f7d1bd2974273a7ebbd0b4ae9514b577f9fbf964
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-05 20:14:56 +0100

    task 2 commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
 import Footer from './components/Footer';
@@ -8,33 +5,11 @@ import MainContent from './components/MainContent';
 import Header from './components/Header';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-    <>
-      <Header/>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-      </div>
-      <MainContent />
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
+		<>
+			<Header />
+			<MainContent />
 			<WelcomeMessage />
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
 			<Footer />
 		</>
 	);
