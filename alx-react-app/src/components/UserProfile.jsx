bfb264923ed7a2aae4675a49a5d57320436557ad
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-13 19:27:14 +0100

    added inline style to some components

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,12 @@
-const UserProfile = (props) => {
+const UserProfile = ({ name, age, bio }) => {
 	return (
-		<div>
-			<h2>{props.name}</h2>
-			<p>Age: {props.age}</p>
-			<p>Bio: {props.bio}</p>
+		<div
+			style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+			<h2 style={{ color: 'blue' }}>{name}</h2>
+			<p>
+				Age:<span style={{ fontWeight: 'bold' }}>{age}</span>
+			</p>
+			<p>Bio: {bio}</p>
 		</div>
 	);
 };
