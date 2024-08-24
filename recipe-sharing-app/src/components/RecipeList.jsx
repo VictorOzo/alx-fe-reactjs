7d8f821e5f6114832b114463db37d35a7dddcd45
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-24 20:24:33 +0100

    more components added

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-	const recipes = useRecipeStore((state) => state.recipes);
+	const { filteredRecipes, filterRecipes } = useRecipeStore((state) => ({
+		filteredRecipes: state.filteredRecipes,
+		filterRecipes: state.filterRecipes,
+	}));
+
+	useEffect(() => {
+		filterRecipes();
+	}, [filterRecipes]);
 
 	return (
 		<div>
-			{recipes.map((recipe) => (
-				<div key={recipe.id}>
-					<h3>{recipe.title}</h3>
-					<p>{recipe.description}</p>
-				</div>
-			))}
+			{filteredRecipes.length > 0 ? (
+				filteredRecipes.map((recipe) => (
+					<div key={recipe.id}>
+						<h3>
+							<Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+						</h3>
+						<p>{recipe.description}</p>
+					</div>
+				))
+			) : (
+				<p>No recipes found.</p>
+			)}
 		</div>
 	);
 };
+
 export default RecipeList;
