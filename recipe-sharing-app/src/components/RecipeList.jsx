5bf71c290d4ca9be610bd52234da1043c67f1f45
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-24 20:41:47 +0100

    Few changes made

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useRecipeStore } from './recipeStore';
+import  useRecipeStore  from './recipeStore';
 
 const RecipeList = () => {
 	const { filteredRecipes, filterRecipes } = useRecipeStore((state) => ({
--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -4,33 +4,28 @@ import {
 	Routes,
 	useParams,
 } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
+import RecipeList from '../components/RecipeList';
+import AddRecipeForm from '../components/AddRecipeForm';
+import RecipeDetails from '../components/RecipeDetails';
+import SearchBar from '../components/SearchBar';
+import FavoritesList from '../components/FavoritesList';
+import RecommendationsList from '../components/RecommendationsList';
 
 function App() {
 	return (
 		<Router>
-			<div>
-				<h1>Recipe Sharing Application</h1>
-				<SearchBar />
-				<AddRecipeForm />
-				<Routes>
-					<Route path='/' element={<RecipeList />} />
-					<Route
-						path='/recipe/:recipeId'
-						element={<RecipeDetailsWrapper />}
-					/>
-					<Route path='/favorites' element={<FavoritesList />} />
-					<Route
-						path='/recommendations'
-						element={<RecommendationsList />}
-					/>
-				</Routes>
-			</div>
+			<h1>Recipe Sharing Application</h1>
+			<SearchBar />
+			<AddRecipeForm />
+			<Routes>
+				<Route path='/' element={<RecipeList />} />
+				<Route
+					path='/recipe/:recipeId'
+					element={<RecipeDetailsWrapper />}
+				/>
+				<Route path='/favorites' element={<FavoritesList />} />
+				<Route path='/recommendations' element={<RecommendationsList />} />
+			</Routes>
 		</Router>
 	);
 }
