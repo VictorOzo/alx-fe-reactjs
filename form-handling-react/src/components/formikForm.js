d0690a1e478d3110f764dcc07261b758b4c7d877
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-29 22:03:21 +0100

    more validation

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,4 +1,4 @@
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
 function formikForm() {
@@ -16,12 +16,31 @@ function formikForm() {
 				// same shape as initial values
 				console.log(values);
 			}}>
-			<Form>
-				<Field name='username' type='text' />
-				<Field name='email' type='email' />
-				<Field name='password' type='password' />
-				<button type='submit'>Submit</button>
-			</Form>
+			{({ errors, touched }) => (
+				<Form>
+					{/* User Name */}
+					<Field name='username' type='text' />
+					{errors.username && touched.username ? (
+						<div>{errors.username}</div>
+					) : null}
+					<ErrorMessage name='username' />
+
+					{/* Email */}
+					<Field name='email' type='email' />
+					{errors.email && touched.email ? (
+						<div>{errors.email}</div>
+					) : null}
+					<ErrorMessage name='email' />
+
+					{/* Password */}
+					<Field name='password' type='password' />
+					{errors.password && touched.password ? (
+						<div>{errors.password}</div>
+					) : null}
+					<ErrorMessage name='password' />
+					<button type='submit'>Submit</button>
+				</Form>
+			)}
 		</Formik>
 	);
 }
