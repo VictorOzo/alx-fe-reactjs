59c2f87a7cf4942fd9405039625f837a89068a33
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-30 23:15:13 +0100

    components created

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
+import Profile from './components/Profile';
 
 function App() {
-	return <>React Router Advanced</>;
+	return (
+		<>
+			React Router Advanced
+			<Profile />
+		</>
+	);
 }
 
 export default App;
---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
+
+const Profile = () => {
+	return (
+		<div>
+			Profile
+			<ProfileSettings />
+			<ProfileDetails />
+		</div>
+	);
+};
+
+export default Profile;
