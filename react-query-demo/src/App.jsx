fade96670235b6597a13e2e1bf5f312899cc8070
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-30 21:31:44 +0100

    fetching data using react query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import PostsComponent from './components/PostsComponent';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,5 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
 const PostsComponent = () => {
-	return <div>PostsComponent</div>;
+	const { isPending, error, data } = useQuery({
+		queryKey: ['postsData'],
+		queryFn: () =>
+			fetch('https://jsonplaceholder.typicode.com/posts').then(
+				(res) => res.json
+			),
+	});
+	if (isPending) return 'Loading...';
+	if (error) return 'An Error has occurred: ' + error.message;
+	return (
+		<>
+			<h1>Posts Component</h1>
+			<h2>{data.title}</h2>
+			<p>{ data.body}</p>
+		</>
+	);
 };
 
 export default PostsComponent;
