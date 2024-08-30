2768462599a903a2164b65c7c1cf294ad589ffe1
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-30 21:51:05 +0100

    errors corrected

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 const PostsComponent = () => {
-	const { isLoading, isError, data } = useQuery({
+	const { isLoading, isError, data, error } = useQuery({
 		queryKey: ['fetchPosts'],
 		queryFn: () =>
 			fetch('https://jsonplaceholder.typicode.com/posts').then(
@@ -9,13 +9,17 @@ const PostsComponent = () => {
 			),
 	});
 	if (isLoading) return 'Loading...';
-	if (isError) return 'An Error has occurred: ' + isError.message;
+	if (isError) return 'An Error has occurred: ' + error.message;
 	return (
-		<>
-			<h1>Posts Component</h1>
-			<h2>{data.title}</h2>
-			<p>{data.body}</p>
-		</>
+		<div>
+			<h1>Post Content</h1>
+			{data.map((post) => (
+				<div key={post.id}>
+					<h1>{post.title}</h1>
+					<p>{post.body}</p>
+				</div>
+			))}
+		</div>
 	);
 };
 
