70d37349e9f31e327d40a7ad7e8e144eb2911b28
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-08-30 21:34:52 +0100

    changes made

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,20 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 
 const PostsComponent = () => {
-	const { isPending, error, data } = useQuery({
-		queryKey: ['postsData'],
+	const { isLoading, isError, data } = useQuery({
+		queryKey: ['fetchPosts'],
 		queryFn: () =>
 			fetch('https://jsonplaceholder.typicode.com/posts').then(
 				(res) => res.json
 			),
 	});
-	if (isPending) return 'Loading...';
-	if (error) return 'An Error has occurred: ' + error.message;
+	if (isLoading) return 'Loading...';
+	if (isError) return 'An Error has occurred: ' + isError.message;
 	return (
 		<>
 			<h1>Posts Component</h1>
 			<h2>{data.title}</h2>
-			<p>{ data.body}</p>
+			<p>{data.body}</p>
 		</>
 	);
 };
