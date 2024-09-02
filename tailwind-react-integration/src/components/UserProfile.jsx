d43563cc69a4fc13b8b339b56f061ce4d5a55dda
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-02 21:09:07 +0100

    more changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
 	return (
-		<div className='user-profile bg-gray-100 p-8 sm:p-4 md:p-8  sm:max-w-sm md:max-w-xs  mx-auto my-20 rounded-lg shadow-lg'>
+		<div className='user-profile bg-gray-100 p-8 sm:p-4 md:p-8  sm:max-w-xs md:max-w-sm  mx-auto my-20 rounded-lg shadow-lg'>
 			<img
 				className='rounded-full w-36 h-36 mx-auto sm:w-36 sm:h-36 md:w-36 md:h-36'
 				src='https://via.placeholder.com/150'
