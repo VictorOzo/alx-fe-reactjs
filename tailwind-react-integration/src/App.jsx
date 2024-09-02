af21ce4acc33440ad052d455b35c76a0180c254b
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-02 20:56:54 +0100

    styles added

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import UserProfile from './components/UserProfile';
 
 function App() {
 	return (
 		<>
 			<h1 className='text-2xl'>Tailwind Css</h1>
+			<UserProfile />
 		</>
 	);
 }
