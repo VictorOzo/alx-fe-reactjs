27030768bfb22c35160676372eec5dfac69c40b6
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-09-20 18:58:09 +0100

    search logic added

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,51 +1,9 @@
-import  { useState, } from 'react';
-import axios from 'axios'; // Import axios for API requests
+import Search from "./components/Search";
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [userData, setUserData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsLoading(true); 
-
-    setError(null); // Clear any previous errors
-
-    try {
-      const response = await axios.get(`https://api.github.com/users/${searchTerm}`);
-      setUserData(response.data);
-    } catch (error) {
-      setError("Looks like we can't find the user.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
+export const App = () => {
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={searchTerm} onChange={handleChange} placeholder="Search GitHub Username" />
-        <button type="submit">Search</button>
-      </form>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {userData && (
-        <div>
-          <img src={userData.avatar_url} alt={userData.login} />
-          <p>{userData.login}</p>
-          <a href={userData.html_url} target="_blank" rel="noreferrer noopener">
-            View Profile
-          </a>
-        </div>
-      )}
+      <Search />
     </div>
   );
 };
-
-export default Search;
